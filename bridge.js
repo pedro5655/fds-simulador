97f961df19b76fa60ea77fdbf3a9c56d70503f7d
Pedro Lameirão <pedro@example.com>
2025-12-10 22:37:21 -0300

    chore: remove remaining build config files from repo

--- bridge.js
+++
@@ -1,9 +0,0 @@
-import { Filesystem, Directory, Encoding } from "@capacitor/filesystem";
-import { Share } from "@capacitor/share";
-
-window.CapacitorPlugins = {
-  Filesystem,
-  Directory,
-  Encoding,
-  Share,
-};
